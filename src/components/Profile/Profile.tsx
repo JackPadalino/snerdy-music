5109dd2654e3fbc1e8936529f41c410515ad18cb
JackPadalino <pada0867@gmail.com>
2023-07-27 12:30:16 -0400

    added new Profile component

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+
+const Profile = () => {
+  const { user } = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+  return <div></div>;
+};
+
+export default Profile;
