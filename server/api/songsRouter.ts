e5b1b71c6f2a2db8450500f1b4d4e1eefda27d8a
JackPadalino <pada0867@gmail.com>
2023-10-19 18:46:24 -0400

    sorting songs alphabetically on api call

--- server/api/songsRouter.ts
+++ server/api/songsRouter.ts
@@ -35,7 +35,9 @@ router.get(
 // GET /api/songs - Get all songs from DB
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const allSongs: SongModelAttributes[] = await Song.findAll();
+    const allSongs: SongModelAttributes[] = await Song.findAll({
+      order: [["artist", "ASC"]],
+    });
     if (!allSongs) {
       res.sendStatus(404);
     } else {
